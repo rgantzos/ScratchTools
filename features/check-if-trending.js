30bca50b0fd4ef6191de1c2ef55bce6b0642cf0a
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-31 13:35:21 -0700

    Update check-if-trending.js

--- features/check-if-trending.js
+++ features/check-if-trending.js
@@ -4,11 +4,11 @@ if (window.location.href.startsWith('https://scratch.mit.edu/projects/')) {
     async function checkTrending() {
         if (document.querySelector('div.share-date') !== null) {
             waitForShareDate.disconnect()
-        var response = await fetch("https://api.scratch.mit.edu/explore/projects?limit=40&offset=0&language=en&mode=trending&q=*")
-        var data = await response.json()
+        let response = await fetch("https://api.scratch.mit.edu/explore/projects?limit=40&offset=0&language=en&mode=trending&q=*")
+        let data = await response.json()
         data.forEach(function(el, i) {
             if (el.id.toString() === window.location.href.replace('https://scratch.mit.edu/projects/', '').replaceAll('/', '')) {
-                var span = document.createElement('span')
+                let span = document.createElement('span')
                 span.textContent = ` • #${i+1} on Trending`
                 document.querySelector('div.share-date').appendChild(span)
             }
